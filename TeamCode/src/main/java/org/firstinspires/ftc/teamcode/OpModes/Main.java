af254025df54e886a4d390b29d0a8f37cbc07f11
gvl610 <68118236+raspiduino@users.noreply.github.com>
2024-06-17 22:55:12 +0700

    Add sensitivity filter for joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Main.java
@@ -3,16 +3,23 @@ import com.qualcomm.robotcore.eventloop.EventLoop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Subsystems.Drivebase;
+
 @TeleOp(name = "main")
 public class Main extends LinearOpMode {
+    private double SENSE = 0.05;
+    
     private Drivebase drive;
+
+    private double sense(double val) {
+        return (Math.abs(val) >= SENSE) ? val : 0;
+    }
 
     public void runOpMode() {
         drive = new Drivebase(hardwareMap);
         waitForStart();
         while(opModeIsActive()) {
-            double ly = -gamepad1.left_stick_y;
-            double rx = gamepad1.right_stick_x;
+            double ly = sense(-gamepad1.left_stick_y);
+            double rx = sense(gamepad1.right_stick_x);
             drive.setPowerToDrive(ly, rx);
         }
     }
